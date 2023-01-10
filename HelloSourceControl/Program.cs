b3801bdd92eb97b02993a53461f13e305816a940
Bart Ault <115667577+Bault496@users.noreply.github.com>
2023-01-10 12:54:06 -0800

    Added some validation for the calc

--- HelloSourceControl/Program.cs
+++ HelloSourceControl/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Data;
 using System.Transactions;
 
 Console.WriteLine("Hello, GitHub Source Control!");
@@ -17,15 +18,40 @@ int total = 0;
 Console.WriteLine("Enter two numbers you would like to add together");
 Console.Write("Enter the first number: ");
 numInput = Console.ReadLine();
-num1 = Convert.ToInt32(numInput);
-Console.Write("Enter the second number: ");
-numInput = Console.ReadLine();
-num2 = Convert.ToInt32(numInput);
-total = addNumbers(num1, num2);
-Console.WriteLine("The total of the two numbers is: " + total);
+if (isValidInt(numInput))
+{
+    num1 = Convert.ToInt32(numInput);
+    Console.Write("Enter the second number: ");
+    numInput = Console.ReadLine();
+    if (isValidInt(numInput))
+    {
+        num2 = Convert.ToInt32(numInput);
+        total = addNumbers(num1, num2);
+        Console.WriteLine("The total of the two numbers is: " + total);
+    } 
+    else
+    {
+        numInput = "Please enter a valid number for the second number.";
+        Console.WriteLine(numInput);
+    }
+   
+}
+else
+{
+    numInput = "Please enter a valid number for the first number.";
+    Console.WriteLine(numInput);
+}
+
 
 // basic method
 // don't need public or private before methods
-int addNumbers(int num1, int num2){
+int addNumbers(int num1, int num2)
+{
     return num1 + num2;
+}
+
+bool isValidInt(string input)
+{
+    int a;
+    return int.TryParse(input, out a);
 }
