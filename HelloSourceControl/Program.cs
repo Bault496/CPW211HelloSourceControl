6f262bc1c458b8be363e890abb8f879596c97198
Bart Ault <115667577+Bault496@users.noreply.github.com>
2023-01-10 12:36:15 -0800

    Added an input that asks the user their name.

--- HelloSourceControl/Program.cs
+++ HelloSourceControl/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Transactions;
+
 Console.WriteLine("Hello, GitHub Source Control!");
+
+// basic input
+string name = "";
+Console.WriteLine("Enter your name: ");
+Console.ReadLine();
+Console.WriteLine("Hi, " + name + "!");
